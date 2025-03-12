be2341770c437a438ddad0927e087543298501dd
Joseph D. Galloway II <josephgalloway520@gmail.com>
2025-03-12 07:53:18 -0500

    Created function prototypes, working on functions in source file

--- project_I/functions.c
+++ project_I/functions.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "functions.h"
 
 int main() {
 
+  int rounds = 0;
+  int p1_points = 0;  // Initialize points for player 1
+  int p2_points = 0;  // Initialize points for player 2
+  int* p1_total = &p1_points;  // Total points for player 1
+  int* p2_total = &p2_points; // Total points for player 2
+
+  enum CHAMPION c1 = createChampion();  // Decide champion for player 1
+  enum CHAMPION c2 = createChampion();  // Decide champion for player 2
+
+
+  printf("Enter the number of rounds: ");
+  scanf("%d", &rounds);
+  printf("\n");
   
+  for (int i = 0; i < rounds; i++) {
+    printf("ROUND %d\n", i);
+    printf("________\n");
+    // Call playRound();
+  }
+
   return 0;
 }
+
+
+int getRandomNumber(int min, int max) {
+  return rand() % (max - min + 1) + min;
+}
+
+enum CHAMPION createChampion() {
+  int randNum = getRandomNumber(1, 3);
+  if (randNum == 0) {
+    return MAGE;
+  }
+  else if (randNum == 1) {
+    return FIGHTER;
+  }
+  else {
+    return TANK;
+  }
+}
+
+// void playRound(int* p1_total, int* p2_total, enum CHAMPION c1, int c1_points, enum CHAMPION c2, int c2_points) {
+
+// }
--- project_I/functions.h
+++ project_I/functions.h
@@ -1,6 +1,24 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+enum CHAMPION {MAGE, FIGHTER, TANK};
 
+// Computes random number between min and max, inclusive, and returns it
+int getRandomNumber(int min, int max);
+
+// Decides the type of the Champion based on the probability values mentioned in games rules
+// (33.33% each for MAGE, FIGHTER, and TANK)
+enum CHAMPION createChampion();
+
+// Calculates and returns the points associated with the given champion type
+int getChampionPoints(enum CHAMPION c);
+
+// Prints out the champion type as a string when passed as an enum
+void printChampion(enum CHAMPION c);
+
+// Plays the game according to the rules 
+// Takes input as the info for the current round
+// Player points are updated directly as pointers
+void playRound(int* p1_total, int* p2_total, enum CHAMPION c1, int c1_points, enum CHAMPION c2, int c2_points);
 
 #endif
